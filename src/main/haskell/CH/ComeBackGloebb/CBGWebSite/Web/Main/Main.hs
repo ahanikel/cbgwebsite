dbdda49ebdb8cfe6618366b7acfa6cdc5fe8b4ff
Axel Hanikel <ahanikel@gmail.com>
2016-06-13 00:28:54 +0200

    Allow http requests but redirect (302) them to https.

--- src/main/haskell/CH/ComeBackGloebb/CBGWebSite/Web/Main/Main.hs
+++ src/main/haskell/CH/ComeBackGloebb/CBGWebSite/Web/Main/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -- CBG
 import qualified CH.ComeBackGloebb.CBGWebSite.Repo.Impl.FileStorage as N
 import           CH.ComeBackGloebb.CBGWebSite.Repo.Repository
@@ -5,30 +7,45 @@ import           CH.ComeBackGloebb.CBGWebSite.Web.Impl.CBGWebSite
 
 -- Yesod
 import           Control.Concurrent                                 (newMVar)
-import           Network.HTTP.Conduit                               (Manager, conduitManagerSettings,
-                                                                     newManager)
+import           Network.HTTP.Conduit                               (Manager,
+                                                                     newManager,
+                                                                     tlsManagerSettings)
+import           Network.HTTP.Types                                 (status302)
+import           Network.Wai                                        (isSecure,
+                                                                     rawPathInfo,
+                                                                     responseLBS)
 import           Network.Wai.Handler.Warp                           (defaultSettings,
                                                                      setPort)
-import           Network.Wai.Handler.WarpTLS                        (runTLS,
+import           Network.Wai.Handler.WarpTLS                        (OnInsecure (..),
+                                                                     onInsecure,
+                                                                     runTLS,
                                                                      tlsSettings)
 import           Yesod                                              (toWaiApp,
                                                                      warp)
 import           Yesod.Static                                       (static)
 
+-- other
 import           Control.Monad.Logger                               (runStderrLoggingT)
 import           Control.Monad.Trans                                (liftIO)
+import qualified Data.ByteString                                    as B
+import qualified Data.ByteString.Lazy                               as BL
 import           Data.Text                                          (pack)
 import           Database.Persist.Sqlite                            (runMigration,
                                                                      runSqlPool,
                                                                      withSqlitePool)
 
-main = do sem                <- newMVar True
-          staticSettings     <- static "static"
-          manager            <- newManager conduitManagerSettings
-          clientId           <- readFile "google.clientId"
-          clientSecret       <- readFile "google.clientSecret"
-          let warpSettings    = setPort 8080 defaultSettings
-              warpTlsSettings = tlsSettings "server.crt" "server.key"
+main = do sem                          <- newMVar True
+          staticSettings               <- static "static"
+          manager                      <- newManager tlsManagerSettings
+          clientId                     <- readFile "google.clientId"
+          clientSecret                 <- readFile "google.clientSecret"
+          let warpSettings              = setPort 8080 defaultSettings
+              warpTlsSettings           = (tlsSettings "server.crt" "server.key") { onInsecure = AllowInsecure }
+              redirectHttp app req resp = if isSecure req
+                                          then app req resp
+                                          else resp $ responseLBS status302 [
+                                            ("Location", B.concat ["https://test.comebackgloebb.ch", rawPathInfo req])
+                                          ] BL.empty
 
           runStderrLoggingT $ withSqlitePool (pack "cbgusers.sqlite") 10 $ \pool -> do
             runSqlPool (runMigration migrateAll) pool
@@ -43,4 +60,4 @@ main = do sem                <- newMVar True
                     (pack clientId)
                     (pack clientSecret)
                     pool
-              runTLS warpTlsSettings warpSettings app
+              runTLS warpTlsSettings warpSettings (redirectHttp app)
